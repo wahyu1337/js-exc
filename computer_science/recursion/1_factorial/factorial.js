fc2157965ee934dfbf2fa2ba910e8d3f539b5527
wahyu1337 <wahyuisnaeni2002@gmail.com>
2026-05-30 22:59:46 +0800

    Finish Computer Science Factorial exercises

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,7 +1,19 @@
 const factorial = function(n) {
     // base case recursive
-    if (n <= 1) return  1;
+    if (n < 0) {
+        return undefined;
+    } else if (n <= 1 && n >= 0) {
+        // case if the value negative number
+        return 1
+    } else if (Number.isFinite(n) && !Number.isInteger(n)) {
+        // case if the value float number
+        return undefined;
+    } else if (typeof n !== "number") {
+        // case if the value is string
+        return undefined;
+    }
 
+    // do the recursive.
     return n * factorial(n - 1);
 };
 
