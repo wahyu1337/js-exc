f5c3e08a892ece90408aec67f5f6acd2ddd26b8a
unknown <wahyuisnaeni2002@gmail.com>
2026-05-31 20:38:05 +0800

    update 02 contains *still wrong

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,11 @@
-const contains = function() {
-  
+const contains = function(obj, value) {
+    // base case.
+    return (Object.values(obj).some(val => {
+        if (val === value) return true;
+        if (val & typeof val === "object") return contains(val, value);
+        return false;
+    }));
+
 };
   
 // Do not edit below this line
