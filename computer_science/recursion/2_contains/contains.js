50aaaef39522433fc789844cef52a9074c643ef9
wahyu1337 <wahyuisnaeni2002@gmail.com>
2026-06-02 20:47:36 +0800

    Update contains exercise

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,12 +1,22 @@
-const contains = function(obj, value) {
-    // base case.
-    return (Object.values(obj).some(val => {
-        if (val === value) return true;
-        if (val & typeof val === "object") return contains(val, value);
-        return false;
-    }));
+    const contains = function(obj, value) {
+        // base case.
+        if (obj === value){
+            return true;
+        }    
 
-};
-  
-// Do not edit below this line
-module.exports = contains;
+        // if the current value NaN (reference)
+        if(Number.isNaN(value) && Number.isNaN(obj)) {
+            return true;
+        }
+
+        // if the current value is not an object or its null., then stop explore.
+        if (typeof obj !== "object" || obj === null){
+            return false;
+        };
+
+        return Object.values(obj).some(nestedValue => contains(nestedValue, value));
+    };
+    
+    // Do not edit below this line
+    module.exports = contains;
+    
