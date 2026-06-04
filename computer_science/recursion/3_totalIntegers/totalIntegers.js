ebf0dda9b3959a50e15acdfd4a62e68522b167ef
unknown <wahyuisnaeni2002@gmail.com>
2026-06-04 19:41:45 +0800

    updaed recursion 3 * still not finish

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,17 +1,21 @@
 const totalIntegers = function(targetValue) {
-    // guard clause.
-    if (typeof targetValue !== "object" && targetValue === null) {
+    let count = 0;
+    
+    // guard case
+    if (typeof targetValue !== "object" || targetValue === null){
         return undefined;
-    }
+    };
 
-    // return if it an integer
-    if (Number.isInteger(targetValue)) {
-        return temps.push(targetValue);
-    }
-
-    // call the recursive
-
-     
+    // recursive proses
+    Object.values(targetValue).forEach(val => {
+        // case if its hit an integer
+        if (Number.isInteger(val)) {
+            count++;
+        } else if (typeof val === "object" && val !== null) {
+            // case if it's value is an object and not a null
+            count += totalIntegers(val);
+        }
+    })
 };
   
 // Do not edit below this line
