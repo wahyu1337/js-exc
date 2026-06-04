56d33c72301cdd9602041ba8959b786f7bcd3ca9
wahyu1337 <wahyuisnaeni2002@gmail.com>
2026-06-04 18:15:30 +0800

    Update Total integers *not finish yet

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,5 +1,17 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(targetValue) {
+    // guard clause.
+    if (typeof targetValue !== "object" && targetValue === null) {
+        return undefined;
+    }
+
+    // return if it an integer
+    if (Number.isInteger(targetValue)) {
+        return temps.push(targetValue);
+    }
+
+    // call the recursive
+
+     
 };
   
 // Do not edit below this line
